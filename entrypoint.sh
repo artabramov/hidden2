8e6e096de47cf6e142402b08da5bdd70e246a059
Artem Abramov <notdepot@gmail.com>
2026-03-03 05:22:33 +0000

    Draft commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-GOCRYPTFS_SECRETS_DIR="/etc/hidden"
-GOCRYPTFS_CIPHERDIR="/var/lib/hidden/encrypted"
-GOCRYPTFS_MOUNTPOINT="/var/lib/hidden/decrypted"
-
-GOCRYPTFS_PASSPHRASE_FILENAME="gocryptfs.key"
-GOCRYPTFS_PASSPHRASE_LENGTH=80
-GOCRYPTFS_CONFIG_FILENAME="gocryptfs.conf"
+set -eu
 
 mkdir -p "$GOCRYPTFS_SECRETS_DIR" "$GOCRYPTFS_CIPHERDIR" "$GOCRYPTFS_MOUNTPOINT"
 
 GOCRYPTFS_PASSPHRASE_PATH="${GOCRYPTFS_SECRETS_DIR}/${GOCRYPTFS_PASSPHRASE_FILENAME}"
-GOCRYPTFS_CONFIG_PATH="${GOCRYPTFS_CIPHERDIR}/${GOCRYPTFS_CONFIG_FILENAME}"
+GOCRYPTFS_CONFIG_PATH="${GOCRYPTFS_CIPHERDIR}/gocryptfs.conf"
 
 # First boot: generate passphrase once (if missing)
 if [ ! -f "$GOCRYPTFS_PASSPHRASE_PATH" ]; then
@@ -22,19 +16,19 @@ fi
 
 # Init only once (if missing config)
 if [ ! -f "$GOCRYPTFS_CONFIG_PATH" ]; then
-  gocryptfs -init -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR"
+  gocryptfs -nosyslog -init -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR"
   echo "[gocryptfs] initialized: $GOCRYPTFS_CIPHERDIR"
 fi
 
 # Mount if not mounted
 if command -v mountpoint >/dev/null 2>&1; then
   if ! mountpoint -q "$GOCRYPTFS_MOUNTPOINT"; then
-    gocryptfs -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR" "$GOCRYPTFS_MOUNTPOINT"
+    gocryptfs -nosyslog -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR" "$GOCRYPTFS_MOUNTPOINT"
     echo "[gocryptfs] mounted: $GOCRYPTFS_MOUNTPOINT"
   fi
 # Mount without pre-check (mountpoint utility is not available)
 else
-  gocryptfs -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR" "$GOCRYPTFS_MOUNTPOINT"
+  gocryptfs -nosyslog -passfile "$GOCRYPTFS_PASSPHRASE_PATH" "$GOCRYPTFS_CIPHERDIR" "$GOCRYPTFS_MOUNTPOINT"
   echo "[gocryptfs] mounted: $GOCRYPTFS_MOUNTPOINT"
 fi
 
