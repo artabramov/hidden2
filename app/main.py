ed5479d3740af9ee3201b8fb60751fb90d001352
Artem Abramov <notdepot@gmail.com>
2026-03-11 18:26:24 +0000

    Added CORS

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ from app.models.folder import Folder
 from app.models.user import User
 
 from app.logging import init_logging
+from starlette.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from app.middleware.request_log_middleware import request_log_middleware
 from app.middleware.request_uuid_middleware import request_uuid_middleware
 
@@ -27,15 +29,22 @@ app = FastAPI(
     title="hidden",
     lifespan=lifespan,
 )
-app.middleware("http")(request_uuid_middleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(GZipMiddleware)
 app.middleware("http")(request_log_middleware)
+app.middleware("http")(request_uuid_middleware)
 
 
 @app.get("/")
 async def root(
     orm_repo: ORMRepository = Depends(get_orm_repository),
 ):
-    user = User(email="email15@noreply.no", name="name15")
+    user = User(email="email20@noreply.no", name="name20")
     await orm_repo.insert(user, commit=True)
     selected_user = await orm_repo.select(User, name="name5")
     return {"app": "hidden", "status": "ok"}
